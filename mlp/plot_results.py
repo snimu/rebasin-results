f0a1ce833e76a8cbbaf0e8daee1bd9d8cceb3a58
snimu <sebastian.nicolas.mueller@gmail.com>
2023-06-01 12:47:55 +0200

    Update plot_results.py

--- mlp/plot_results.py
+++ mlp/plot_results.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+import copy
 import os
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 
@@ -61,6 +63,129 @@ def plot_pcd_results() -> None:
     )
 
 
+def plot_mm_hf() -> None:
+    results_2_100 = pd.read_csv("results/merge-many/results_hf_2-100.csv")
+    savefile = "results/merge-many/merge_many_hf_2-100.png"
+    xkey = "hidden_features"
+    xaxislabel = "Hidden Features"
+    title = "MergeMany: weight_decay=0.0, num_models=3"
+
+    plot_one_mm_plot(results_2_100, title, savefile, xkey, xaxislabel)
+
+    results_2_100 = normalize(results_2_100, "loss_merged", "acc_merged")
+    savefile = "results/merge-many/merge_many_hf_2-100_norm.png"
+    title = "MergeMany: weight_decay=0.0, num_models=3 (normalized)"
+
+    plot_one_mm_plot(results_2_100, title, savefile, xkey, xaxislabel)
+
+    results_100_2000 = pd.read_csv("results/merge-many/results_hf_100-2000.csv")
+    savefile = "results/merge-many/merge_many_hf_100-2000.png"
+    xkey = "hidden_features"
+    xaxislabel = "Hidden Features"
+    title = "MergeMany: weight_decay=0.0, num_models=3"
+
+    plot_one_mm_plot(results_100_2000, title, savefile, xkey, xaxislabel)
+
+    results_100_2000 = normalize(results_100_2000, "loss_merged", "acc_merged")
+    savefile = "results/merge-many/merge_many_hf_100-2000_norm.png"
+    title = "MergeMany: weight_decay=0.0, num_models=3 (normalized)"
+
+    plot_one_mm_plot(results_100_2000, title, savefile, xkey, xaxislabel)
+
+
+def plot_mm_wd() -> None:
+    results_lin = pd.read_csv("results/merge-many/results_wd_0.0-0.1.csv")
+    savefile = "results/merge-many/merge_many_wd_0.0-0.1.png"
+    xkey = "weight_decay"
+    xaxislabel = "Weight Decay"
+    title = "MergeMany: hidden_features=100, num_models=3"
+
+    plot_one_mm_plot(results_lin, title, savefile, xkey, xaxislabel)
+
+    results_lin = normalize(results_lin, "loss_merged", "acc_merged")
+    savefile = "results/merge-many/merge_many_wd_0.0-0.1_norm.png"
+    title = "MergeMany: hidden_features=100, num_models=3 (normalized)"
+
+    plot_one_mm_plot(results_lin, title, savefile, xkey, xaxislabel)
+
+    results_log = pd.read_csv("results/merge-many/results_wd_0.0-0.1_log.csv")
+    savefile = "results/merge-many/merge_many_wd_0.0-0.1_log.png"
+    xkey = "weight_decay"
+    xaxislabel = "Weight Decay"
+    title = "MergeMany: hidden_features=100, num_models=3"
+
+    plot_one_mm_plot(results_log, title, savefile, xkey, xaxislabel)
+
+    results_log = normalize(results_log, "loss_merged", "acc_merged")
+    savefile = "results/merge-many/merge_many_wd_0.0-0.1_log_norm.png"
+    title = "MergeMany: hidden_features=100, num_models=3 (normalized)"
+
+    plot_one_mm_plot(results_log, title, savefile, xkey, xaxislabel)
+
+
+def plot_one_mm_plot(
+        results: pd.DataFrame, title: str, savefile: str, xkey: str, xaxislabel: str
+) -> None:
+    fig, axs = plt.subplots(1, 2, figsize=(8, 5))
+    fig.subplots_adjust(top=0.9, bottom=0.17)
+
+    fig.suptitle(title)
+
+    axs[0].plot(
+        results[xkey],
+        results["loss_avg"],
+        label="loss avg", color="blue"
+    )
+    axs[0].plot(
+        results[xkey],
+        results["loss_merged"],
+        label="loss merged", color="red"
+    )
+    axs[0].set_title("Loss")
+    axs[0].set_ylabel("Loss")
+    axs[0].grid()
+
+    axs[1].plot(
+        results[xkey],
+        results["acc_avg"],
+        label="acc avg", color="blue"
+    )
+    axs[1].plot(
+        results[xkey],
+        results["acc_merged"],
+        label="acc merged", color="red"
+    )
+    axs[1].set_title("Accuracy")
+    axs[1].set_ylabel("Accuracy")
+    axs[1].grid()
+
+    axs[0].set_xlabel(xaxislabel)
+    axs[1].set_xlabel(xaxislabel)
+
+    fig.legend(
+        labels=["avg model", "merged model"],
+        loc="lower center",
+        ncol=2,
+        labelcolor=["blue", "red"],
+    )
+
+    # Save here
+    fig.savefig(savefile, dpi=300)
+    plt.cla()
+    plt.clf()
+    plt.close(fig)
+
+
+def normalize(data: pd.DataFrame, key_loss: str, key_acc: str) -> pd.DataFrame:
+    ndata = copy.deepcopy(data)
+    norm_factors_loss = ndata[key_loss].values
+    norm_factors_acc = ndata[key_acc].values
+    ndata["loss_avg"] = ndata["loss_avg"] / norm_factors_loss
+    ndata["loss_merged"] = ndata["loss_merged"] / norm_factors_loss
+    ndata["acc_avg"] = ndata["acc_avg"] / norm_factors_acc
+    ndata["acc_merged"] = ndata["acc_merged"] / norm_factors_acc
+    return ndata
+
+
 if __name__ == "__main__":
-    plot_pcd_results()
-    plt.savefig("pcd-results.png", dpi=300)
+    plot_mm_wd()
